09ee20ba78ed69a032bbf3ff825de4da83fad500
Albametnet <alhernandezsan@gmail.com>
2018-08-08 18:29:34 +0200

    everything ok

--- src/App.js
+++ src/App.js
@@ -1,18 +1,79 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor (props) {
+    super (props);
+
+    this.filterName = this.filterName.bind(this);
+
+    this.state = {
+      people :[],
+      name: ''
+    }  
+    
+}
+componentDidMount () {
+  this.getPeople();
+  
+}
+  getPeople () {
+    fetch(
+      'http://hp-api.herokuapp.com/api/characters'
+    )
+      .then(function (response) {
+        return response.json();
+      })
+      .then((json) => {
+        const characters = json;
+
+
+        let newCharacters= [] 
+          for ( let i=0; i<characters.length; i++) {
+            newCharacters[i] = {
+              ...characters[i],
+              id: i,
+            }
+        }
+        this.setState({
+          people: newCharacters
+        })
+      })
+  }
+
+  filterName(e) {
+    const myperson = e.currentTarget.value.toLowerCase();
+    this.setState({
+      name: myperson
+    });
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Harry Potter Characters</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <main>
+        <input type="text" onChange= {this.filterName} />
+        <ul> {this.state.people
+        .filter(item => {
+          return item.name.toLowerCase().includes(this.state.name);
+        })
+        .map (item =>{
+          return (
+            <li>
+            <div>
+             <img src={item.image} alt="foto del personaje"/>
+             <h2 >{item.name} </h2>
+             <p>{item.house}</p>
+             <p>{item.id}</p>
+            </div>
+            </li>
+          )
+        })}  
+        </ul>
+        </main>
       </div>
     );
   }
