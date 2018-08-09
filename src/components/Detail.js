a0598e6408125086db6ba1341599ef7a41a8daa6
Albametnet <alhernandezsan@gmail.com>
2018-08-09 11:06:03 +0200

    todos los datos en tarjeta

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -1,27 +1,34 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 // import CharacterCard from './Detail';
 
 class Detail extends React.Component {
   render() {
 
-const myCharacter= this.props.people[this.props.match.params.id]
+    const myCharacter = this.props.people[this.props.match.params.id]
 
-     
+    let state = '';
+    if (myCharacter.alive == true) {
+      state = 'Alive';
+    }
+    else {
+      state = 'Muerto 💀';
+    }
 
-     
     return (
       <div>
         <Link to="/" className="back-btn">Volver</Link>
-        <p>kjbkjb 
-        </p>
-        <p>{myCharacter.name}</p>
+        <img src={myCharacter.image} alt="foto del personaje" />
+        <h2>{myCharacter.name}</h2>
+        <p>House: {myCharacter.house}</p>
+        <p>Birthdate: {myCharacter.dateOfBirth}</p>
+        <p>Patronus: {myCharacter.patronus}</p>
+        <p>Life state : {state}</p>
+
       </div>
 
     );
   }
 }
-// }
-
 export default Detail;
