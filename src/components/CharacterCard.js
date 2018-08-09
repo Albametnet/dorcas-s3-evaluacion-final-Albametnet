1c33b66faea28272a3a69adf183e0a7135ab7a27
Albametnet <alhernandezsan@gmail.com>
2018-08-09 13:19:11 +0200

    almost done

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -8,21 +8,37 @@ class Detail extends React.Component {
     const myCharacter = this.props.people[this.props.match.params.id]
 
     let state = '';
-    if (myCharacter.alive == true) {
+    if (myCharacter.alive === true) {
       state = 'Alive';
     }
     else {
       state = 'Muerto 💀';
     }
     return (
-      <div>
-        <Link to="/" className="back-btn">Volver</Link>
-        <img src={myCharacter.image} alt="foto del personaje" />
-        <h2>{myCharacter.name}</h2>
-        <p>House: {myCharacter.house}</p>
-        <p>Birthdate: {myCharacter.dateOfBirth}</p>
-        <p>Patronus: {myCharacter.patronus}</p>
-        <p>Life state : {state}</p>
+      <div className="character__page">
+        <div className="character__card">
+          <div className="character__card-cover" style={{ backgroundImage: `url(${myCharacter.image})` }}>
+            <img className="character__card-img" src={myCharacter.image} alt="foto del personaje" />
+          </div>
+          <div className="character__card-texts">
+            <div className="character__card-name">
+              <h2>{myCharacter.name}</h2>
+            </div>
+            <div className="character__card-noname">
+            <p>House: {myCharacter.house}</p>
+            <p>Birthdate: {myCharacter.dateOfBirth}</p>
+            <p>Patronus: {myCharacter.patronus}</p>
+            <p>Life state : {state}</p>
+            </div>
+            <div className="return__button">
+            <Link to="/" className="back-btn">Volver</Link>
+          </div>
+          </div>
+          
+        </div>
+
+
+
 
       </div>
 
